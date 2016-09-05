8686c8c59b1c9abad9dca4d670439830f369e042
Dimas Mahbiali <dimasmahbialay@gmail.com>
2016-09-05 12:40:29 +0700

    Praktikum 1
    Praktikum 1

---
+++ LED_blink.ino
@@ -0,0 +1,17 @@
+int led = 13;
+
+void setup() {
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(led, HIGH);
+  delay(100);
+  digitalWrite(led, LOW);
+  delay(100);
+  digitalWrite(led, HIGH);
+  delay(10000);
+  digitalWrite(led, LOW);
+  delay(10000);
+}
+
